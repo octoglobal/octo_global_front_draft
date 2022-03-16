e6a63531ac84fd436e34b681bc92fa43ade7c23c
Rodion Pyatigroskiy <81239058+NenadoPL1Z@users.noreply.github.com>
2022-03-16 17:52:10 +0500

    architecture-v1

---
+++ pages/blog/news/[id].tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {NextPage} from "next";
+
+const Post: NextPage = () => {
+    return (
+        <h1>
+            Пост
+        </h1>
+    );
+};
+
+export default Post;
