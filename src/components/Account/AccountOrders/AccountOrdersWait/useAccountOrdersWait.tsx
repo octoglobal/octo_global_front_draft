a228a4d25bdbcf9224603eda86b7e8dfe05efc14
Rodion Pyatigroskiy <81239058+NenadoPL1Z@users.noreply.github.com>
2022-04-25 10:42:02 +0500

    account

--- src/components/Account/AccountOrders/AccountOrdersWait/useAccountOrdersWait.tsx
+++ src/components/Account/AccountOrders/AccountOrdersWait/useAccountOrdersWait.tsx
@@ -1,33 +1,49 @@
 import {useAppDispatch, useAppSelector} from '@/hooks/useReduxHooks';
-import {useEffect, useMemo} from 'react';
+import {useEffect, useMemo, useState} from 'react';
 import {fetchOrderWaitData} from '@/reducers/orderWaitSlice/asyncThunk/orderWaitApi';
 import {useForm} from 'react-hook-form';
 import {useUserStore} from '@/hooks/useUserStore';
+import {octoAxios} from '@/lib/http';
 
 export const useAccountOrdersWait = () => {
+
+	const {
+		isAdmin,
+	} = useUserStore();
+
 	const {
 		page,
 		pageLimit,
 		orderWaitData,
 	} = useAppSelector(state => state.orderWaitReducer);
-	const {
-		isAdmin
-	} = useUserStore();
+
 	const dispatch = useAppDispatch();
-
-
 	const methods = useForm();
+	const [isModal, setIsModal] = useState<boolean>(false);
 
 	const isDataLength = useMemo(() => (
 		!!(orderWaitData.length && Array.isArray(orderWaitData))
 	), [orderWaitData]);
 
-	const dropDownItems = useMemo(() => {
-		if (isAdmin) {
-			return [];
-		}
-		return  [];
-	}, [isAdmin]);
+	const handleToggleModal = () => {
+		setIsModal(prevState => !prevState);
+	};
+
+	const handleDeleteOrder = () => {
+		// const sendData = {
+		// 	'userId': 66,
+		// 	'orderId': 1
+		// };
+		// octoAxios.delete('/admin/orders', {
+		// 	data: sendData
+		// }).then(r => console.log(r));
+		handleToggleModal();
+	};
+
+	const dropDownData = [
+		{title: 'Удалить', onClick: handleToggleModal},
+		{title: 'Перенести', onClick: () => console.log(1)},
+	];
 
 
 	useEffect(() => {
@@ -36,10 +52,41 @@ export const useAccountOrdersWait = () => {
 		}
 	}, []);
 
+	const dialogStyles = {
+		'& .MuiDialog-container': {
+			'& .MuiPaper-root': {
+				minWidth: '704px',
+				maxHeight: '353px',
+				height: '100%',
+				width: '100%',
+				'& > div': {
+					height: '100%',
+					padding: '115px 10px',
+					textAlign: 'center',
+					'& > div': {
+						'& > h4': {
+							marginBottom: '70px',
+						},
+						'& > button': {
+							width: '65px !important',
+							padding: '0',
+							minWidth: '65px !important',
+						}
+					}
+				},
+			}
+		}
+	};
+
 	return {
+		isModal,
+		isAdmin,
 		methods,
+		dialogStyles,
+		dropDownData,
 		isDataLength,
 		orderWaitData,
-		dropDownItems,
+		handleDeleteOrder,
+		handleToggleModal,
 	};
 };
