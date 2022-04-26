46f37c32ab2b7404baff22c1b37a377937fd6654
AxiixAAA <slavae22@mail.ru>
2022-04-26 10:30:36 +0500

    Fix
    1. Перенос строк в отзывах
    2. Исправление отрисовки времени в отзывах

--- src/components/Account/AccountOrders/AccountOrdersAdd/AccountAddModal/AccountAddModal.tsx
+++ src/components/Account/AccountOrders/AccountOrdersAdd/AccountAddModal/AccountAddModal.tsx
@@ -1,7 +1,5 @@
 import React, { FC } from 'react';
 import ModalUI from '../../../../../UI/UIComponents/ModalUI/ModalUI';
-import { styled } from '@mui/material';
-import ButtonUI from '../../../../../UI/UIComponents/ButtonUI/ButtonUI';
 
 interface IAccountAddModalProps {
 	open: boolean;
@@ -19,16 +17,16 @@ const AccountAddModal: FC<IAccountAddModalProps> = ({ open, onClose }) => {
 	);
 };
 
-const ButtonContainerMUI = styled('div')(() => ({
-	maxWidth: '135px',
-	margin: '68px auto 0',
-	'& > button': {
-		opacity: 0.8,
-		fontWeight: 400,
-		minWidth: '135px',
-		height: '32px',
-		marginBottom: 0,
-	}
-}));
+// const ButtonContainerMUI = styled('div')(() => ({
+// 	maxWidth: '135px',
+// 	margin: '68px auto 0',
+// 	'& > button': {
+// 		opacity: 0.8,
+// 		fontWeight: 400,
+// 		minWidth: '135px',
+// 		height: '32px',
+// 		marginBottom: 0,
+// 	}
+// }));
 
 export default React.memo(AccountAddModal);
