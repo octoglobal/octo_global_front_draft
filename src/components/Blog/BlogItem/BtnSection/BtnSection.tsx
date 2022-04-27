6a055edf5185bc80e12d55978e9ed5592a25862a
Evgeny_Kiselev <mor-cre@mail.ru>
2022-04-27 09:16:00 +0500

    добавил модальное окно на удаление

--- src/components/Blog/BlogItem/BtnSection/BtnSection.tsx
+++ src/components/Blog/BlogItem/BtnSection/BtnSection.tsx
@@ -1,9 +1,10 @@
-import React, { FC } from 'react';
+import React, { FC,useState } from 'react';
 import { useBtnSectionStyles } from '@/components/Blog/BlogItem/BtnSection/style';
 import BascetIcon from '../../../../UI/UIIcon/Basket.svg';
 import EditPencilBlueIcon from '../../../../UI/UIIcon/EditPencilBlue.svg';
 
 import {useUserStore} from '@/hooks/useUserStore';
+import ModalConfirmUI from 'UI/UIComponents/ModalConfirmUI/ModalConfirmUI';
 
 interface IBtnSection {
 	id:number
@@ -11,23 +12,42 @@ interface IBtnSection {
 
 
 const BtnSection: FC<IBtnSection> = ({id})=>{
-
-	console.log('id в BtnSection',id);
+	const [openConfirmDialog, setOpenConfirmDialog] = useState(false);
+	
 	const {
 		isAdmin
 	} = useUserStore();
 
+
+	const handlerDialogOpen = () => {
+		console.log('handlerDialogState: openModal');
+		setOpenConfirmDialog(true);
+	};
+	const handlerCancel = () : void => {
+		console.log('handlerDialogState: closeModal');
+		setOpenConfirmDialog(false);
+	};
 	return (
 		<>
 			{ isAdmin? <BtnSectionMUI>
-				<ButtonIconMUI>
+				<ButtonIconMUI onClick={handlerDialogOpen}>
 					<BascetIcon/>
 				</ButtonIconMUI>
 					
-				<ButtonIconMUI>
+				<ButtonIconMUI >
 					<EditPencilBlueIcon/>
 				</ButtonIconMUI>
+				
+					
+				
 			</BtnSectionMUI>: null}
+			<ModalConfirmUI 
+			
+				open={openConfirmDialog}
+				title="Вы уверены что хотите удалить пост?"
+				onClickYes={handlerCancel}
+				onClickNo={handlerCancel}
+			/>
 		</>
 	
 		
