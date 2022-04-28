baf47ff2d8e64824a26be196f83525c3722b9637
Evgeny_Kiselev <mor-cre@mail.ru>
2022-04-28 15:54:30 +0500

    рефакторинг

--- src/store/reducers/blogSlice/blogSlice.ts
+++ src/store/reducers/blogSlice/blogSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction, current } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import {IBlogModel} from '@/models/IBlogModel';
 import { fetchAddNewsBlog, fetchNewsData, fetchUpdateBlog,fetchDeleteBlogItem} from '@/reducers/blogSlice/asyncThunk/blogApi';
 
