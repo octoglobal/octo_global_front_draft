f9706e8dbb7b5b7fcb1490489431b45bd3ab6448
Evgeny_Kiselev <mor-cre@mail.ru>
2022-05-12 15:59:35 +0500

    рефакторинг

--- src/components/AboutUsPage/AboutUsPage.tsx
+++ src/components/AboutUsPage/AboutUsPage.tsx
@@ -1,18 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import HeaderMargin from '@/components/AnyPage/Header/HeaderMargin/HeaderMargin';
-import { styled } from '@mui/material';
-import CollapseInfo from '@/components/AnyPage/CollapseInfo/CollapseInfo';
-import VpnPage from './questionPages/VpnPage';
-import TranslatePage from './questionPages/TranslatePage';
-import AboutTransferPage from './questionPages/AboutTransferPage';
-import { useRouter } from 'next/router';
 import { useAboutUsStyle } from './style';
 
 
-
-const AboutUsPage = () => {
-	
-
+const AboutUsPage = () => {	
 
 	return (
 		<ContainerMUI>
@@ -34,18 +25,14 @@ const AboutUsPage = () => {
 				Наша основная цель — это индивидуальный подход к каждому клиенту, профессионализм и компетентность.
 				Octo global - ваш персональный надежный помощник по доставке товаров из Европы!
 			</ParagraphMUI>
+			
 		</ContainerMUI>
 	);
 };
 
-
-const ContainerMUI = styled('div')(() => ({
-	maxWidth: '650px',
-	margin: '15px auto 0'
-}));
-
 const {
-	ParagraphMUI
+	ParagraphMUI,
+	ContainerMUI
 } = useAboutUsStyle();
 
 export default React.memo(AboutUsPage);
--- src/components/AboutUsPage/style.ts
+++ src/components/AboutUsPage/style.ts
@@ -2,13 +2,17 @@ import { styled } from '@mui/material';
 
 export const useAboutUsStyle = () => {
 
-	const ParagraphMUI = styled('p')(({theme}) => ({
+	const ParagraphMUI = styled('p')(() => ({
 		fontSize: '18px',
 		fontWeight: 300,
 		marginBottom: 20
 	}));
-
+	const ContainerMUI = styled('div')(() => ({
+		maxWidth: '650px',
+		margin: '15px auto 0'
+	}));
 	return {
+		ContainerMUI,
 		ParagraphMUI
 	};
 };
