3d16ee44f23b7ea575dd2cba4198989c54193bc3
LeraKill <kilinav25@gmail.com>
2022-05-25 13:53:51 +0500

    create bg-wrapper, html adaptive

--- src/components/AboutUsPage/AboutUsPage.tsx
+++ src/components/AboutUsPage/AboutUsPage.tsx
@@ -2,37 +2,37 @@ import React from 'react';
 import HeaderMargin from '@/components/AnyPage/Header/HeaderMargin/HeaderMargin';
 import { useAboutUsStyle } from './style';
 
-
-const AboutUsPage = () => {	
-
+const AboutUsPage = () => {
 	return (
 		<ContainerMUI>
-			<HeaderMargin/>
-					
-			<ParagraphMUI>
-				Компания &quot;Octo Global&quot; работает на российском и немецком рынках с 2021 года.
-			</ParagraphMUI>
-
-			<ParagraphMUI>
-				Головной офис компании находится в Германии в городе Гамбург, а официальный представитель в России г. Санкт-Петербург. 
-				Наш основной склад расположен в городе Гамбург, дополнительные склады находятся в Роттердаме (Нидерланды) и Котке (Финляндия).
-			</ParagraphMUI>
-			<ParagraphMUI>
-				Наше основное направление – это предоставление склада для ваших покупок в Европе и дальнейшая логистика.
-			</ParagraphMUI>
-			<ParagraphMUI>
-				Мы современная и динамично развивающаяся компания.
-				Наша основная цель — это индивидуальный подход к каждому клиенту, профессионализм и компетентность.
-				Octo global - ваш персональный надежный помощник по доставке товаров из Европы!
-			</ParagraphMUI>
-			
+			<HeaderMargin />
+			<AboutContainerMUI>
+				<AboutTitleMUI>
+				О нас
+				</AboutTitleMUI>
+				<ParagraphMUI>
+					<HighlightSpanMUI> Компания &quot;Octo Global&quot;</HighlightSpanMUI> работает на российском и немецком рынках с <HighlightSpanMUI> 2021 </HighlightSpanMUI> года. 
+				</ParagraphMUI>
+				<ParagraphMUI>
+					Головной офис компании находится в Германии в городе <HighlightSpanMUI>Гамбург</HighlightSpanMUI>, а
+					официальный представитель в России <HighlightSpanMUI>г. Санкт-Петербург</HighlightSpanMUI>. Наш
+					основной склад расположен в городе Гамбург, дополнительные
+					склады находятся в Роттердаме (Нидерланды) и Котке (Финляндия).
+				</ParagraphMUI>
+				<ParagraphMUI>
+					Наше основное направление – это <HighlightSpanMUI>предоставление склада для ваших
+					покупок</HighlightSpanMUI> в Европе и дальнейшая логистика.
+				</ParagraphMUI>
+				<ParagraphMUI>
+					Мы <HighlightSpanMUI>современная</HighlightSpanMUI> и динамично <HighlightSpanMUI>развивающаяся  </HighlightSpanMUI> компания. Наша основная
+					цель — это индивидуальный подход к каждому клиенту,
+					профессионализм и компетентность. Octo global - ваш персональный <HighlightSpanMUI>надежный </HighlightSpanMUI> помощник по доставке товаров из Европы!
+				</ParagraphMUI>
+			</AboutContainerMUI> 
 		</ContainerMUI>
 	);
 };
 
-const {
-	ParagraphMUI,
-	ContainerMUI
-} = useAboutUsStyle();
+const { ContainerMUI, AboutContainerMUI, AboutTitleMUI, ParagraphMUI, HighlightSpanMUI,} = useAboutUsStyle();
 
 export default React.memo(AboutUsPage);
--- src/components/AboutUsPage/style.ts
+++ src/components/AboutUsPage/style.ts
@@ -1,18 +1,62 @@
 import { styled } from '@mui/material';
 
 export const useAboutUsStyle = () => {
-
-	const ParagraphMUI = styled('p')(() => ({
-		fontSize: '18px',
-		fontWeight: 300,
-		marginBottom: 20
-	}));
 	const ContainerMUI = styled('div')(() => ({
-		maxWidth: '650px',
-		margin: '15px auto 0'
+		maxWidth: '778px',
+		margin: '25px auto 0',
+	}));
+	const AboutContainerMUI = styled('div')(( {theme})=>({
+		maxWidth: '778px',
+		margin: '117px auto 200px',
+		minHeight: '530px',
+		[theme.breakpoints.down(1360)]:{
+			margin:'60px auto 80px',
+		},
+		
+		[theme.breakpoints.down(961)]:{
+			margin:'40px 0px 70px 0px',
+			padding: '0px 28px',
+		},
+		[theme.breakpoints.down(391)]:{
+			margin:'40px 0px 70px 0px',
+			padding: '0px 27px',
+		}
+	}));
+	const AboutTitleMUI = styled('h1')(({theme}) => ({
+		fontSize: '32px',
+		fontWeight: 400,
+		textAlign:'center',
+		lineHeight:'117%',
+		margin: '0px 0px 25px 0px',
+		padding: '0px 0px 0px 0px',
+		[theme.breakpoints.down(961)]:{
+			fontSize: '28px',
+		},
+		[theme.breakpoints.down(391)]:{
+			fontSize: '24px',
+		}
+	}));
+	const ParagraphMUI = styled('p')(({theme}) => ({
+		fontSize: '20px',
+		fontWeight: 300,
+		marginBottom: 20,
+		lineHeight: '117%',
+		textAlign: 'justify',
+		[theme.breakpoints.down(391)]:{
+			fontSize: '18px',
+		},
+		[theme.breakpoints.down(391)]:{
+			fontSize: '16px',
+		},
+	}));
+	const HighlightSpanMUI = styled('span')(()=>({
+		color: 'rgba(39, 77, 130, 1)'
 	}));
 	return {
 		ContainerMUI,
-		ParagraphMUI
+		AboutContainerMUI,
+		AboutTitleMUI,
+		ParagraphMUI,
+		HighlightSpanMUI,
 	};
 };
