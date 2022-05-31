b0d5ba659b3e7bf8ec9ddd544ba9066991e20c42
Evgeny_Kiselev <mor-cre@mail.ru>
2022-05-31 11:26:08 +0500

    изменил 404зфпу

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {NextPage} from 'next';
 import HeaderLayout from '@/layout/HeaderLayout/HeaderLayout';
+import HeaderMargin from '@/components/AnyPage/Header/HeaderMargin/HeaderMargin';
 const NotFound: NextPage = () => {
 	return (
 		<HeaderLayout>
+			<HeaderMargin/>
 			<div>
-				not found
+				404 Not found
 			</div>
 		</HeaderLayout>
 	);
