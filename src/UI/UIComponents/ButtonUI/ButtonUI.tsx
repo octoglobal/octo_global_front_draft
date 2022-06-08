022a015f07f071958522b8cb9c634f7e9ee5d994
Evgeny_Kiselev <mor-cre@mail.ru>
2022-06-08 09:45:38 +0500

    вынес стили из ButtonUI

--- src/UI/UIComponents/ButtonUI/ButtonUI.tsx
+++ src/UI/UIComponents/ButtonUI/ButtonUI.tsx
@@ -1,37 +1,27 @@
-import React, {FC, useMemo} from 'react';
-import {ButtonProps, SxProps} from '@mui/material';
+import React, { FC, useMemo } from 'react';
+import { ButtonProps, SxProps } from '@mui/material';
 
-import {useMobile} from '@/hooks/useMedia';
+import { useMobile } from '@/hooks/useMedia';
 
-import {useButtonUIStyle} from './style';
+import { useButtonUIStyle } from './style';
 
-const ButtonUI : FC<ButtonProps> = (props) => {
-
-	const {isMobile} = useMobile();
-
-	const {
-		CustonButtonUI,
-		ButtonStyle,
-		ButtonMobileStyle
-	} = useButtonUIStyle();
+const ButtonUI: FC<ButtonProps> = (props) => {
+	const { isMobile } = useMobile();
 
 	const buttonStyle = useMemo(() => {
 		const propsStyles = props?.sx ? props.sx : {};
 		const adaptiveStyles = isMobile ? ButtonMobileStyle : ButtonStyle;
 		return {
 			...adaptiveStyles,
-			...propsStyles
+			...propsStyles,
 		};
 	}, [isMobile]);
 
 	return (
-		<CustonButtonUI
-			{...props}
-			sx={buttonStyle as SxProps}
-		>
+		<CustonButtonUI {...props} sx={buttonStyle as SxProps}>
 			{props.children}
 		</CustonButtonUI>
 	);
 };
-
+const { CustonButtonUI, ButtonStyle, ButtonMobileStyle } = useButtonUIStyle();
 export default ButtonUI;
