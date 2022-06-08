0970aeb9429e36247ffc9e9383d58404ef863ca3
Evgeny_Kiselev <mor-cre@mail.ru>
2022-06-08 10:31:05 +0500

    убрал ограничение на длину трек номера

--- src/components/AnyPage/OrderItem/OrderStatusModal/useOrderStatusModal.ts
+++ src/components/AnyPage/OrderItem/OrderStatusModal/useOrderStatusModal.ts
@@ -1,17 +1,16 @@
-import {FieldValues, SubmitHandler, useForm} from 'react-hook-form';
-import {useEffect, useMemo} from 'react';
-import {octoAxios} from '@/lib/http';
-import {IOrderModel} from '@/models/IOrderModel';
-import {IDefaultFetchSuccess} from '../../../../types/types';
-import {ComponentType} from '@/components/AnyPage/OrderItem/OrderItem';
-import {useAppDispatch} from '@/hooks/useReduxHooks';
-import {fetchChangeStatusPackageToSend} from '@/reducers/orderStockSlice/asynThunk/stockApi';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { useEffect, useMemo } from 'react';
+import { octoAxios } from '@/lib/http';
+import { IOrderModel } from '@/models/IOrderModel';
+import { IDefaultFetchSuccess } from '../../../../types/types';
+import { ComponentType } from '@/components/AnyPage/OrderItem/OrderItem';
+import { useAppDispatch } from '@/hooks/useReduxHooks';
+import { fetchChangeStatusPackageToSend } from '@/reducers/orderStockSlice/asynThunk/stockApi';
 
 interface IFormStatus {
 	trackNumber: string;
-	orderStatus: { title: string, value: number };
+	orderStatus: { title: string; value: number };
 }
-
 
 export const useOrderStatusModal = (
 	isOpen: boolean,
@@ -65,12 +64,12 @@ export const useOrderStatusModal = (
 		controller: {
 			name: 'trackNumber',
 			control: methods.control,
-			rules: {required: true, maxLength: 14, pattern: /^[a-zA-Z0-9]+$/g},
+			rules: { required: true, pattern: /^[a-zA-Z0-9]+$/g },
 		},
 		inputProps: {
 			placeholder: 'Трек номер',
 			disabled: component === 'wait' || component === 'stock',
-		}
+		},
 	};
 
 	useEffect(() => {
@@ -79,45 +78,54 @@ export const useOrderStatusModal = (
 		}
 	}, [orderItem, isOpen, packageChange]);
 
-
 	const onSubmit: SubmitHandler<IFormStatus | FieldValues> = (data) => {
-		if (data?.trackNumber && (data?.orderStatus?.value !== undefined || submitStatus !== undefined)) {
+		if (
+			data?.trackNumber &&
+			(data?.orderStatus?.value !== undefined ||
+				submitStatus !== undefined)
+		) {
 			try {
 				const trackNumber = methods.getValues('trackNumber');
 				if (packageChange) {
-					dispatch(fetchChangeStatusPackageToSend({
-						userId: orderItem.userId,
-						packageId: orderItem.id,
-						trackNumber: trackNumber
-					}))
-						.then(r => {
-							if (r.payload) {
-								const data = r.payload as {message: 'success'};
-								if (data.message == 'success') {
-									successCallback();
-								}
+					dispatch(
+						fetchChangeStatusPackageToSend({
+							userId: orderItem.userId,
+							packageId: orderItem.id,
+							trackNumber: trackNumber,
+						})
+					).then((r) => {
+						if (r.payload) {
+							const data = r.payload as { message: 'success' };
+							if (data.message == 'success') {
+								successCallback();
 							}
-						});
-				} else {
-					const statusId = data?.orderStatus?.value ? data?.orderStatus?.value : submitStatus;
-					octoAxios.post<IDefaultFetchSuccess>('/admin/orders', {
-						userId: orderItem.userId,
-						track_number: trackNumber ? trackNumber : orderItem.trackNumber,
-						title: orderItem.title,
-						comment: orderItem.comment,
-						statusId: statusId,
-					}).then(r => {
-						if (r.data.message === 'success') {
-							successCallback();
 						}
 					});
+				} else {
+					const statusId = data?.orderStatus?.value
+						? data?.orderStatus?.value
+						: submitStatus;
+					octoAxios
+						.post<IDefaultFetchSuccess>('/admin/orders', {
+							userId: orderItem.userId,
+							track_number: trackNumber
+								? trackNumber
+								: orderItem.trackNumber,
+							title: orderItem.title,
+							comment: orderItem.comment,
+							statusId: statusId,
+						})
+						.then((r) => {
+							if (r.data.message === 'success') {
+								successCallback();
+							}
+						});
 				}
 			} catch (e) {
 				throw new Error('Error change status');
 			}
 		}
 	};
-
 
 	useEffect(() => {
 		if (!isOpen) {
@@ -132,6 +140,6 @@ export const useOrderStatusModal = (
 		methods,
 		onSubmit,
 		collapseItems,
-		trackNumberProps
+		trackNumberProps,
 	};
 };
