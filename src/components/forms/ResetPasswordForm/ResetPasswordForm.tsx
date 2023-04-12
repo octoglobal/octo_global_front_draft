e57cec5f1b4d98880f229b812b11c1ccc312c683
NenadoPL1Z <limon4ikpro1@gmail.com>
2023-04-12 14:12:33 +0500

    update: reset text

--- src/components/forms/ResetPasswordForm/ResetPasswordForm.tsx
+++ src/components/forms/ResetPasswordForm/ResetPasswordForm.tsx
@@ -17,15 +17,15 @@ const ResetPasswordForm = () => {
 		onSubmit,
 		openModal,
 		changeOpenModal,
-		
+
 	} = useResetPasswordForm(setError,reset);
-	
+
 	const {
 		FormsWrapperBox,
 		FormsInput,
 		FormsButton,
 		FormsDescription,
-		
+
 	} = useFormsStyle();
 
 	return (
@@ -33,8 +33,10 @@ const ResetPasswordForm = () => {
 			<FormsWrapperBox>
 
 				<FormsDescription>
-					<Typography variant="body2">
-						Сообщение для смены пароля будет отправленно на вашу электронную почту
+					<Typography variant="body2" style={{whiteSpace: 'pre-wrap'}}>
+						Здравствуйте!{'\n'}
+Для восстановления пароля введите свой электронный адрес, указанный при регистрации, и через несколько минут на Ваш e-mail придёт письмо с Вашими регистрационными данными и контрольной строкой для ввода нового пароля.
+Для того, чтобы сбросить пароль, перейдите по следующей ссылке.
 					</Typography>
 				</FormsDescription>
 
@@ -61,7 +63,7 @@ const ResetPasswordForm = () => {
 						Продолжить
 					</ButtonUI>
 				</FormsButton>
-				
+
 				<ModalUI
 					dialogProps={{
 						open: openModal,
